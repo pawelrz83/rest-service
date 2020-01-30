7e8a18f4606b6ea7543d72f7abb60383083a08f8
Piotr Rzeczkowski <pierre.riviere.pl@gmail.com>
2020-01-30 01:01:28 +0100

    Created two rest functions authenticate1 and authenticate2, added 2 tasks to work alone

--- rest-service/src/main/java/com/example/restservice/Authenticate.java
+++ rest-service/src/main/java/com/example/restservice/Authenticate.java
@@ -1,47 +1,52 @@
 package com.example.restservice;
 
-
 import java.security.SecureRandom;
 import java.util.Base64;
 
 public class Authenticate {
 
+	private final long id;
+	private final String validPin = "1234";
+	private final String pin;
+	private String token="error";
+	
+	public Authenticate(long id, String pin) {
+		this.id = id;
+		this.pin = pin;
+		System.out.println("Constructor initialising");
+		System.out.println("validPin is: " + this.validPin);
+		System.out.println("pin is: " + this.pin);
+		//condition to check authentication by pin
+		if(this.pin.equals(this.validPin))
+		{
+			System.out.println("Token generation");
+			this.token = generateToken();
+		}
+	}
 
-private final long id;
-private final String validPin = "1234";
-private final String pin;
-private String token="error";
-
-public Authenticate(long id, String pin) {
-this.id = id;
-this.pin = pin;
-System.out.println("Constructor initialising");
-System.out.println("validPin is: " + this.validPin);
-System.out.println("pin is: " + this.pin);
-//condition to check authentication by pin
-if(this.pin.equals(this.validPin))
-{
-System.out.println("Token generation");
-this.token = generateToken();
-}
-}
-
-public long getId() {
-return id;
-}
-
-public String generateToken()
-{
-Base64.Encoder base64Encoder = Base64.getUrlEncoder();
-SecureRandom random = new SecureRandom();
-byte bytes[] = new byte[80];
-   random.nextBytes(bytes);
-String token = base64Encoder.encodeToString(bytes);
-return token;
-}
-
-public String getContent() {
-System.out.println("Getting content method");
-return token;
-}
+	public long getId() {
+		return id;
+	}
+	
+	public String getToken() {
+		return token;
+	}
+	public String generateToken()
+	{
+		Base64.Encoder base64Encoder = Base64.getUrlEncoder(); 
+		SecureRandom random = new SecureRandom();
+		byte bytes[] = new byte[80];
+	    random.nextBytes(bytes);
+		String token = base64Encoder.encodeToString(bytes);
+		return token;
+	}
+	/*
+	 * Removing this function eliminates 'content' from JSON, so this serialization proces must 
+	 * encapsulate all get* function from java class and creates a JSON based on them
+	public String getContent() {
+		System.out.println("Getting content method");
+		//return token;
+		//instead of token we return JSON with {token: value} 
+		return String.format("{'token': '%s'}",token);
+	}*/
 }
--- rest-service/src/main/java/com/example/restservice/AuthenticateController.java
+++ rest-service/src/main/java/com/example/restservice/AuthenticateController.java
@@ -1,19 +1,52 @@
 package com.example.restservice;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticateController {
 
-private static final String template = "Hello, %s!";
-private final AtomicLong counter = new AtomicLong();
+	private final AtomicLong counter = new AtomicLong();
 
-@GetMapping("/authenticate")
-public Authenticate authenticate(@RequestParam(value = "pin") String name) {
-return new Authenticate(counter.incrementAndGet(), name);
-}
+	//First function uses just serialization and returns JSON with all fields made by get functions
+	@GetMapping("/authenticate1")
+	@ResponseBody
+	public Authenticate authenticate1(@RequestParam(value = "pin") String name) {
+		return new Authenticate(counter.incrementAndGet(), name);
+	}
+
+	//Second one just throws json created manually by hash map and adding key pair "token" : "value" 
+	//where value is taken from getToken method on object authenticator which was initialized with pin before.
+	@GetMapping("/authenticate2")
+	@ResponseBody
+	public Map<String, Object> authenticate2(@RequestParam(value = "pin") String name) 
+	{
+		Map<String, Object> json_result = new LinkedHashMap<>();
+		Authenticate authenticator = new Authenticate(counter.incrementAndGet(), name);
+	    json_result.put("token", authenticator.getToken());
+
+	    return json_result;
+		
+	}
+	
+	//Write another requestBody /authenticate3 where you check if pin is ok and you return json with id, token but 
+	//if pin is not valid, return json {'status':'401', 'msg':'Unauthorized'}  not less and no more than this information
+	
+	//If we have multiple GetMapping("/functionXYZ") and each time we construct this return json object, we may need to create 
+	//another class like Result.java which would have constructor :
+	//public Result(long statusCode) inside where there would be condition to check what;s the status code is
+	// if statusCode==401 then return {'status':401,'msg':'Unauthorized'}
+	// if statusCode==200 then return {'status':200}
+	// if statusCode==404 then return {'status':404, 'msg':'Not found'}
+	// if statusCode==500 then return {'status':500, 'msg':'Internal Error'}
+	//All these errors are common HTTP status codes well known in internet. It could be used as a builder for json_result 
+	//to not mess with the same steps each time
+	
+	
 }
